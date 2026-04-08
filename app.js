899ecb7ba7068951fbe0537f14b74ea39c8d3543
Raphael Centelles <raphaelcentelles@gmail.com>
2026-04-08 11:11:37 +0200

    feat: page d'accueil avec liste des articles

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ app.set('view engine', 'ejs');
 app.listen(3000, () => {
   console.log('Serveur lancé sur le port 3000');
 });
+
+app.get('/', (req, res) => {
+    const sql = `
+        SELECT 
+            Article.Id_article, 
+            Article.Titre, 
+            LEFT(Article.contenu, 100) AS apercu, 
+            Article.Date_creation AS Date_publication, 
+            Auteur.Pseudo AS Nom_auteur, 
+            Categorie.Nom AS Nom_categorie
+        FROM Article
+        INNER JOIN Auteur ON Article.Id_auteur = Auteur.Id_auteur
+        INNER JOIN Categorie ON Article.Id_categorie = Categorie.Id_categorie
+        ORDER BY Article.Date_creation DESC
+    `;
+    connection.query(sql, (error, results) => {
+        if (error) {
+            console.error("Erreur de base de données :", error);
+            return res.status(500).send("Erreur interne du serveur");
+        }
+        res.render('index', { articles: results });
+    });
+});
+
